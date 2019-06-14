383ff3481cc7727d25d5813ecabd72a5f00f078c
Daniel Falbel <dfalbel@gmail.com>
2019-06-14 14:49:28 -0300

    use default installation path

--- R/hello.R
+++ R/hello.R
@@ -5,12 +5,12 @@ tensorflow::install_tensorflow
 #' @export
 install_anaconda_linux <- function() {
   download.file(url = "https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh", destfile = "~/anaconda.sh")
-  system("bash ~/anaconda.sh -b -p $HOME/anaconda")
+  system("bash ~/anaconda.sh -b")
 }
 
 #' @export
 install_anaconda_mac <- function() {
   download.file(url = "https://repo.anaconda.com/archive/Anaconda3-2019.03-MacOSX-x86_64.pkg", destfile = "~/anaconda.sh")
   system("chmod a+x ~/anaconda.sh")
-  system("bash ~/anaconda.sh -b -p $HOME/anaconda")
+  system("bash ~/anaconda.sh -b")
 }
