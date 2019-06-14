3251c630926948d549c5c050649824c7ba27f66a
Daniel Falbel <dfalbel@gmail.com>
2019-06-14 13:28:42 -0300

    respond to the method

--- R/hello.R
+++ R/hello.R
@@ -1,3 +1,15 @@
 #' @importFrom tensorflow install_tensorflow
 #' @export
 tensorflow::install_tensorflow
+
+#' @export
+install_anaconda_linux <- function() {
+  download.file("https://repo.anaconda.com/archive/Anaconda3-latest-Linux-x86_64.sh", "~/anaconda.sh")
+  system("bash ~/anaconda.sh -b -p $HOME/anaconda")
+}
+
+#' @export
+install_anaconda_mac <- function() {
+  download.file("https://repo.anaconda.com/archive/Anaconda2-latest-MacOSX-x86_64.pkg", "~/anaconda.sh")
+  system("bash ~/anaconda.sh -b -p $HOME/anaconda")
+}
--- tests/testthat/helper-installation.R
+++ tests/testthat/helper-installation.R
@@ -1,6 +1,15 @@
 library(tfinstall)
 
 version <- Sys.getenv("TENSORFLOW_VERSION")
+method <- Sys.getenv("INSTALL_METHOD")
+
+if (method=="conda") {
+  if (R.Version()$os=="linux-gnu") {
+    install_anaconda_linux()
+  } else if (grepl("darwin", R.Version()$os)) {
+    install_anaconda_mac()
+  }
+}
 
 if (Sys.getenv("TRAVIS") == "true")
-  install_tensorflow(version = version, restart_session = FALSE)
+  install_tensorflow(version = version, method = method, restart_session = FALSE)
