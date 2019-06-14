4313ceb908254c21009c392780bd584e87ba8995
Daniel Falbel <dfalbel@gmail.com>
2019-06-14 14:06:13 -0300

    allow for a restart after installation

--- R/hello.R
+++ R/hello.R
@@ -4,12 +4,12 @@ tensorflow::install_tensorflow
 
 #' @export
 install_anaconda_linux <- function() {
-  download.file("https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh", "~/anaconda.sh")
+  download.file(url = "https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh", destfile = "~/anaconda.sh")
   system("bash ~/anaconda.sh -b -p $HOME/anaconda")
 }
 
 #' @export
 install_anaconda_mac <- function() {
-  download.file("https://repo.anaconda.com/archive/Anaconda3-2019.03-MacOSX-x86_64.pkg", "~/anaconda.sh")
+  download.file(url = "https://repo.anaconda.com/archive/Anaconda3-2019.03-MacOSX-x86_64.pkg", destfile = "~/anaconda.sh")
   system("bash ~/anaconda.sh -b -p $HOME/anaconda")
 }
