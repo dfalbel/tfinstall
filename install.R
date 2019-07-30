4e69ab60cb4ed2fa9b04603ebc29dbbaadf13b17
Daniel Falbel <dfalbel@gmail.com>
2019-07-30 17:18:46 -0300

    use conda.exe

--- install.R
+++ install.R
@@ -39,9 +39,8 @@ print("Installing -------------")
 if (Sys.getenv("TRAVIS") == "true") {
   install_tensorflow(version = version, method = method, restart_session = FALSE)
 } else if (Sys.getenv("APPVEYOR") == "True") {
-  print(list.files("C:\\Miniconda36-x64\\Scripts\\"))
   install_tensorflow(version = version, method = method, restart_session = FALSE,
-                     conda = "C:\\Miniconda36-x64\\Scripts\\conda.bat")
+                     conda = "C:\\Miniconda36-x64\\Scripts\\conda.exe")
 }
 
 print("Finished installing ------------------")
