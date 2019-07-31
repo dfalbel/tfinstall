de4adb9af6cb990030a64214222ab668af920967
Daniel Falbel <dfalbel@gmail.com>
2019-07-30 21:35:05 -0300

    set condaenv before testing

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,13 +1,3 @@
 library(testthat)
-
-
-if (Sys.getenv("APPVEYOR") == "True") {
-  reticulate::use_condaenv(
-    condaenv = "r-reticulate",
-    conda =  "C:\\Miniconda36-x64\\Scripts\\conda.exe",
-    required = TRUE)
-}
-
-
 library(tfinstall)
 test_check("tfinstall")
